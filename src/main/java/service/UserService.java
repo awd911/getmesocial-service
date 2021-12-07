0ae0747ea0ff7daf0e9b7a2953855ba138528f38
awd911 <tom.milton91@gmail.com>
2021-12-07 15:45:48 -0500

    More Structure changes

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,17 @@
+package service;
+
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUser(){
+        return userRepository.getUser();
+    }
+}
